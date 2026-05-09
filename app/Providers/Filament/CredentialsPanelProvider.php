0ca1bae2419b29d1a67481fe6a0a66146cd52f63
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-05-09 22:00:59 +0200

    refactor(provider): use CredentialResource as default panel
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- app/Providers/Filament/CredentialsPanelProvider.php
+++ app/Providers/Filament/CredentialsPanelProvider.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\Credentials\CredentialResource;
 use Filament\Auth\MultiFactor\App\AppAuthentication;
 use Filament\Auth\MultiFactor\Email\EmailAuthentication;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
@@ -45,7 +45,7 @@ class CredentialsPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
-                Dashboard::class,
+                CredentialResource::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
